d24a5a0f56d1760f03ee9ad739ca4ea667a1e37f
moluskman <thenuka.se@gmail.com>
2026-06-15 20:31:39 +1000

    Bot setup with on_ready event and run command

--- src/main.py
+++ src/main.py
@@ -1,5 +1,16 @@
 import discord
-
 import os
-
 from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print("Jewvis Online")
+
+
+client.run(os.getenv("DISCORD_BOT_TOKEN"))
